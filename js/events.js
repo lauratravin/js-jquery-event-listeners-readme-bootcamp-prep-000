698031a4cef39b8155928fb95ae3341ac3fb0a9c
Laura M. Travin <laura.travin@gmail.com>
2017-12-02 07:57:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 
 function submitIt(){
   $('form').on('submit', function(){
-    alert(`"Your form is going to be submitted now."`);
+    alert('Your form is going to be submitted now.');
     return;
   });
 }
